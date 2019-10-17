19005da3707350b1f40365017f5c0556ec17579b
TPS <trinh.ho@tpssoft.com.vn>
2019-10-17 15:04:28 +0700

    Initial Commit

---
+++ NFCAndChirp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFCAndChirp
+//
+//  Created by TPS on 10/17/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
